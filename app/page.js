87cfb62632105351a7495c6944a959f1bc74bef8
Omar Hassaan <omarhassaan772012@gmail.com>
2026-08-19 08:08:19 +0300

    update Banner component to display hero title and subtitle

--- app/page.js
+++ app/page.js
@@ -1,11 +1,13 @@
 import Image from "next/image";
 import styles from "./page.module.css";
 import Header from "@/components/Header/Header";
+import Banner from "@/components/Banner/Banner";
 
 export default function Home() {
   return (
     <div>
       <Header />
+      <Banner />
     </div>
   );
 }

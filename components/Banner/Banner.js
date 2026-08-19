2ccfcb71c234ff7d0b82e23c38fec793e23587a7
Omar Hassaan <omarhassaan772012@gmail.com>
2026-08-19 10:06:00 +0300

    الله لا اله الا هو الحي القيوم لا تأخذه سنة ولا نوم له ما في السماوات وما في الأرض من ذا الذي يشفع عنده إلا بإذنه يعلم ما بين أيديهم وما خلفهم ولا يحيطون بشيء من علمه إلا بما شاء وسع كرسيه السماوات والأرض ولا يؤوده حفظهما وهو العلي العظيم

--- components/Banner/Banner.js
+++ components/Banner/Banner.js
@@ -1,3 +1,5 @@
+'use client'
+
 import "./Banner.css"
 import Image from "next/image"
 import Link from "next/link"
@@ -6,9 +8,39 @@ import Link from "next/link"
 function Banner() {
     return(
         <div className="banner">
-            <h1 className="hero-title">We are not just building</h1>
-            <h1 className="hero-title2">a digital experience</h1>
-            <h1 className="hero-title3">we are building lasting trust.</h1>
+
+            <div className="object1" data-aos="fade">
+                <div className="p1"></div>
+                <div className="p2">Creative Agency</div>
+            </div>
+
+
+            <div className="object2">
+            <h1 className="hero-title" data-aos="fade-left">We build</h1>
+            <div className="hero-title1" data-aos="fade-left" data-aos-delay="300">
+            <h1 className="hero-title2">a digital</h1>
+            <h1 className="hero-title3">experience</h1>
+            </div>
+            <h1 className="hero-title4" data-aos="fade-left" data-aos-delay="600">and lasting trust.</h1>
+            </div>
+
+            <h5 className="object3" data-aos="fade-left" data-aos-delay="800">Operating at the intersection of cutting-edge technology and high-end artistry. </h5> 
+            <h5 className="object3" data-aos="fade-left" data-aos-delay="800">We transform visionary ideas into immersive digital realities.</h5>
+            
+
+            <div className="badge-wrapper" data-aos="fade-left">
+    <div className="circle-text">
+    <svg viewBox="0 0 100 100" width="140" height="140">
+        <path id="circlePath" d="M 50, 50 m -37, 0 a 37,37 0 1,1 74,0 a 37,37 0 1,1 -74,0" fill="none" />
+        <text font-size="11.5" font-weight="bold" letter-spacing="1.5">
+        <textPath href="#circlePath">
+            • EXPLORE OUR WORK • VELIQUIX STUDIO
+        </textPath>
+        </text>
+        </svg>
+    </div>
+    <div className="center-arrow">↗</div>
+</div>
         </div>
     )
 

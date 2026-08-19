5cef29bc4a604adb706c01cb16e2dc4788a4daa0
Omar Hassaan <omarhassaan772012@gmail.com>
2026-08-19 08:25:03 +0300

    لا اله الا الله محمد رسول الله

--- components/Banner/Banner.js
+++ components/Banner/Banner.js
@@ -6,10 +6,12 @@ import Link from "next/link"
 function Banner() {
     return(
         <div className="banner">
-            <h1 className="hero-title"> We are not just building</h1> <h1 className="hero-title2">a digital experience </h1> <h1 className="hero-title3">we are building lasting trust.</h1>
+            <h1 className="hero-title">We are not just building</h1>
+            <h1 className="hero-title2">a digital experience</h1>
+            <h1 className="hero-title3">we are building lasting trust.</h1>
         </div>
     )
-    
+
 }
 
 export default Banner
